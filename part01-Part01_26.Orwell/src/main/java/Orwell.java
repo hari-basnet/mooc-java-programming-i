42b9c4e9803db5c9803ccaadf7273c51c957c0d8
Hari Basnet <hari.basnet@x-akseli.fi>
2020-06-04 23:00:12 +0300

    orwell

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -7,5 +7,13 @@ public class Orwell {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Enter a number");
+        int number = scan.nextInt();
+        
+        if(number == 1984){
+            System.out.println("Orwell");
+        }else {
+            System.out.println("Try again!!!");
+        }
     }
 }
--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -7,5 +7,13 @@ public class Ancient {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+          System.out.println("Enter a number");
+        int number = scan.nextInt();
+        
+        if(number < 2015 && number > 0){
+            System.out.println("Ancient history!");
+        }else {
+            System.out.println("Try again!!!");
+        }
     }
 }
