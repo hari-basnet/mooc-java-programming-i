ca7bec39d6eb20f0f2493a7eac56c3b07a4a19db
Hari Basnet <hari.basnet@x-akseli.fi>
2020-06-14 21:05:31 +0300

    thought triangle for pyramid

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -29,20 +29,14 @@ public class StarSign {
     public static void printSquare(int size) {
         // second part of the exercise
         for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+            printStars(size);
         }
     }
 
     public static void printRectangle(int width, int height) {
         // third part of the exercise
         for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+            printStars(width);
         }
     }
 
