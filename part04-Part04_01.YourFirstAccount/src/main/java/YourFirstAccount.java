e1c9e83f249838fe0fd0b358f187335f40598557
Hari Basnet <hari.basnet@x-akseli.fi>
2020-06-17 01:57:37 +0300

    oop started and going good

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account hariAccount = new Account("Hari's account", 100.0);
+        hariAccount.deposit(20.0);
+        
+        System.out.println("The balance of Hari's account is " + hariAccount.toString());
     }
 }
