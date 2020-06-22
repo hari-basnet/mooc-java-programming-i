e6315c46227fc7b69fa6694423a5805bc4497357
Hari Basnet <hari.basnet@x-akseli.fi>
2020-06-22 15:31:46 +0300

    part06_01 Menu. lesson learned that it is better to not add duplicate items in ArrayList in some case

--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -10,4 +10,24 @@ public class Menu {
     }
 
     // implement the required methods here
+    
+    public void addMeal(String meal){
+        if(!meal.isEmpty() && !meals.contains(meal)){
+            this.meals.add(meal);
+        }
+    }
+    
+    public void printMeals(){
+        if(this.meals.size() > 0){
+            for(int i = 0; i < meals.size(); i++){
+                System.out.println(meals.get(i));
+            }
+        }
+    }
+    
+    public void clearMenu(){
+        if(this.meals.size() > 0){
+            this.meals.clear();
+        }
+    }
 }
