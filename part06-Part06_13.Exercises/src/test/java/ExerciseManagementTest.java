2bc40b65bc52240a3c1d18e90b7151f8f43e49f6
Hari Basnet <hari.basnet@x-akseli.fi>
2020-07-06 00:49:19 +0300

    part6 completed

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,42 @@
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
 
 
 public class ExerciseManagementTest {
 
+    private ExerciseManagement management;
+    
+    @Before
+    public void initialize(){
+        management = new ExerciseManagement();
+    }
+    
+    @Test
+    public void addExerciseTest(){
+        
+        assertEquals(0, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne(){
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addedItemIsInList(){
+        management.add("Learn more about TDD");
+        assertTrue(management.exerciseList().contains("Learn more about TDD"));
+    }
+    
+    @Test
+    public void exercisesCanBeMarkedAsCompleted(){
+        management.add("Exercise testing");
+        management.markAsCompleted("Exercise testing");
+        assertTrue(management.isCompleted("Exercise testing"));
+    }
 }
