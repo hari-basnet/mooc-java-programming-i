c26c4785522614c58c12a69aa7a5ad567ee684ea
Hari Basnet <hari.basnet@x-akseli.fi>
2020-07-06 18:24:22 +0300

    sorting algorithm done

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -26,10 +26,48 @@ public class MainProgram {
         return smallestIndex;
     }
     
+    public static int indexOfSmallestFrom(int[] table, int startIndex){
+        int smallest = table[startIndex];
+        int smallestIndex = startIndex;
+        for(int i = startIndex; i < table.length; i++){
+            if(table[i] < smallest){
+                smallest = table[i];
+                smallestIndex = i;
+            }
+        }
+        
+        return smallestIndex;
+    }
+    
+    public static void swap(int[] array, int index1, int index2){
+        
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+        
+    }
+    
+    public static void sort(int[] array){
+        
+        for(int i = 0; i < array.length; i++){
+            swap(array, indexOfSmallestFrom(array, i), i);
+            
+        }
+    }
+       
+    
+    
+       
+       
     public static void main(String[] args) {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
         
+        swap(array, 0 , 0);
+        
+        for(int item : array){
+            System.out.print(item + " ");
+        }
     }
 
 }
